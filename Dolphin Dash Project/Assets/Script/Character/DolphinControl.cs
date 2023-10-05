ff5be8c7adb3419a636266ff63a8a7ad726a8786
IXRKS90 <kunal.singh@kompanions.com>
2023-10-05 14:31:51 +0530

    Updated project from main

--- Dolphin Dash Project/Assets/Script/Character/DolphinControl.cs
+++ Dolphin Dash Project/Assets/Script/Character/DolphinControl.cs
@@ -9,16 +9,12 @@ public class DolphinControl : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private TextMeshProUGUI CoinDisplay;
     private Animator anim;
-    private bool m_isInWater;
-    [SerializeField] private CapsuleCollider2D m_CapsuleCollider2D;
-    [SerializeField] private LayerMask SeaLayer;
-    private RaycastHit2D raycastHit;
+
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-        //m_CapsuleCollider2D = GetComponent<CapsuleCollider2D>();
     }
 
 
@@ -41,27 +37,14 @@ public class DolphinControl : MonoBehaviour
         }
     }
 
-    private bool InWaterCheck()
+    private void InWaterCheck()
     {
-        raycastHit = Physics2D.CapsuleCast(m_CapsuleCollider2D.bounds.center, m_CapsuleCollider2D.size, CapsuleDirection2D.Horizontal, 0, Vector2.down, 0.2f, SeaLayer);
-        return raycastHit.collider != null;
+
     }
 
 
     private void OnDrawGizmosSelected()
     {
-        Color TransparentRed = new Color(1, 0, 0, .5f);
-        Color TransparentGreen = new Color(0, 1, 0, .5f);
-
-        if (InWaterCheck())
-        {
-            Gizmos.color = TransparentGreen;
-        }
-        else
-        {
-            Gizmos.color = TransparentRed;
-        }
-
-        Gizmos.DrawSphere(this.transform.position, 1f);
+        
     }
 }
